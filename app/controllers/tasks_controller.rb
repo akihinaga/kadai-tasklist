73d8be0793cd6e3a56d70e8b86843af017eeddf0
hisa.monji <hisamonjipgmail.com>
2018-06-20 08:27:16 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,56 +1,57 @@
-class TasksController < ApplicationController
-   def index
-      @tasks = Task.all
-   end
-   
-   def show
-      @task = Task.find(params[:id])
-   end
-   
-   def new
-      @task = Task.new
-   end
-   
-   def create
-      @task = Task.new(task_params)
-      
-      if @task.save
-         flash[:success] = 'Taskが正常に投稿されました'
-         redirect_to @task
-      else
-         flash.now[:danger] = 'taskが投稿されませんでした'
-         render :new
-      end
-   end
-   
-   def edit
-      @task = Task.find(params[:id])
-   end
-   
-   def update
-     @task = Task.find(params[:id])
+class MessagesController < ApplicationController
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
 
-     if @task.update(task_params)
-       flash[:success] = 'Task は正常に更新されました'
-       redirect_to @task
-     else
-       flash.now[:danger] = 'Task は更新されませんでした'
-       render :edit
-     end
-   end
-   
-   def destroy
-     @task = Task.find(params[:id])
-     @task.destroy
+  def index
+    @messages = Message.all
+  end
 
-     flash[:success] = 'Task は正常に削除されました'
-     redirect_to tasks_url
-   end
-   
-   private
-   
-   def task_params
-      params.require(:task).permit(:content)
-   end
+  def show
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+
+    if @message.save
+      flash[:success] = 'Message が正常に投稿されました'
+      redirect_to @message
+    else
+      flash.now[:danger] = 'Message が投稿されませんでした'
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @message.update(message_params)
+      flash[:success] = 'Message は正常に更新されました'
+      redirect_to @message
+    else
+      flash.now[:danger] = 'Message は更新されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    @message.destroy
+
+    flash[:success] = 'Message は正常に削除されました'
+    redirect_to messages_url
+  end
+
+  private
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
 
