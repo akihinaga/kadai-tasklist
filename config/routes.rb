a44cdfd5067b21d474730266c2d7529f135723f3
hisa.monji <hisamonjipgmail.com>
2018-06-30 14:05:49 +0000

    tasklist top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  get 'toppages/index'
 
-  resources :tasks
+  root to: 'toppages#index'
+
+  get 'login', to: 'tasks#new'
+  post 'login', to: 'tasks#create'
+  delete 'logout', to: 'tasks#destroy'
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
