c77ee6085d008409b9eb822db9b3d12cbeb7c2aa
hisa.monji <hisamonjipgmail.com>
2018-07-06 00:44:55 +0000

    post_tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
   
    def index
       @tasks = Task.order(created_at: :desc).page(params[:page]).per(3)
@@ -13,14 +14,14 @@ class TasksController < ApplicationController
    end
    
    def create
-      @task = Task.new(task_params)
-      
+      @task = current_user.tasks.build(task_params)
       if @task.save
          flash[:success] = 'Taskが正常に投稿されました'
-         redirect_to @task
+         redirect_to root_url
       else
-         flash.now[:danger] = 'taskが投稿されませんでした'
-         render :new
+         @task = current_user.tasks.oeder('created_at DESC').page(params[:page])
+         flash.now[:danger] = 'Taskが投稿されませんでした'
+         render :toppages/index
       end
    end
    
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180702130121) do
+ActiveRecord::Schema.define(version: 20180703135447) do
 
   create_table "relationships", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20180702130121) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "status"
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -39,4 +41,5 @@ ActiveRecord::Schema.define(version: 20180702130121) do
 
   add_foreign_key "relationships", "users"
   add_foreign_key "relationships", "users", column: "follow_id"
+  add_foreign_key "tasks", "users"
 end
