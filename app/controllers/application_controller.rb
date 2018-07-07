2ec272c452ab24baf5a319d5ba760a6056fabc21
hisa.monji <hisamonjipgmail.com>
2018-07-07 12:52:03 +0000

    post_tasklist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,12 @@ class ApplicationController < ActionController::Base
   private
   
   def require_user_logged_in
-      unless logged_in?
-       redirect_to login_url
-      end
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
+  
+  def counts(user)
+    @count_tasks = user.tasks.count
   end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   
    def index
       @tasks = Task.order(created_at: :desc).page(params[:page]).per(3)
@@ -40,7 +41,6 @@ class TasksController < ApplicationController
    
    def destroy
      @task.destroy
-
      flash[:success] = 'Task は正常に削除されました'
      redirect_to tasks_url
    end
@@ -54,4 +54,10 @@ class TasksController < ApplicationController
    def task_params
       params.require(:task).permit(:content, :status)
    end
+   def correct_user
+    @micropost = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
